b40ad04390d31fea4336420f03356260235d9d77
Guido Juliano Ubaldi <guidojulianou@gmail.com>
2024-02-08 09:18:18 -0300

    correcciones en config de ga

--- src/GoogleAnalytics.tsx
+++ src/GoogleAnalytics.tsx
@@ -1,26 +1,18 @@
 'use client';
 
 import Script from 'next/script';
-import * as gtag from '../gtag.js';
 
 const GoogleAnalytics = () => {
   return (
     <>
-      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-                      window.dataLayer = window.dataLayer || [];
-                      function gtag(){dataLayer.push(arguments);}
-                      gtag('js', new Date());
-                      gtag('config', '${gtag.GA_TRACKING_ID}', {
-                      page_path: window.location.pathname,
-                      });
-                    `,
-        }}
-      />
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=G-825R9NKGQD`} />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-825R9NKGQD');
+        `}
+      </Script>
     </>
   );
 };
